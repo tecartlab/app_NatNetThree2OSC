037664a56ee3ce25d96bda9da148ba3cd117fab9
IASpace <ia.space@zhdk.ch>
2022-07-01 15:16:40 +0200

    updated to 8.8.0

---
+++ scripts/Motive3_to_Local.bat
@@ -0,0 +1,1 @@
+D:\00_core\NatNet2OSC\Motive3.x\NatNetThree2OSC --localIP=10.128.96.107 --motiveIP=10.128.96.111 --oscSendIP=10.128.96.107 --oscSendPort=54321 --oscCtrlPort=54322 --oscMode=max --yup2zup=true --autoReconnect=true --dataStreamInfo=2000
