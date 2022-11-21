9057adca5b18ef3df0e26d741ff273d4688b5502
ARRAKIS\IAAdmin <ia.sapce@zhdk.ch>
2022-11-21 19:29:42 +0100

    update to the latest

--- scripts/Motive3_to_Local.bat
+++ scripts/Motive3_to_Local.bat
@@ -1,1 +1,1 @@
-D:\00_core\NatNet2OSC\Motive3.x\NatNetThree2OSC --localIP=10.128.96.107 --motiveIP=10.128.96.111 --oscSendIP=10.128.96.107 --oscSendPort=54321 --oscCtrlPort=54322 --oscMode=max --yup2zup=true --autoReconnect=true --dataStreamInfo=2000
+D:\00_core\NatNet2OSC\Motive3.x\NatNetThree2OSC --localIP=10.128.96.107 --motiveIP=10.128.96.113 --oscSendIP=10.128.96.107 --oscSendPort=54321 --oscCtrlPort=54322 --oscMode=max --yup2zup=true --sendOtherMarkerInfo --sendMarkerInfo --autoReconnect=true --frameModulo=12 --dataStreamInfo=2000
--- update.cmd
+++ update.cmd
@@ -1,1 +1,4 @@
 echo "update NatNet2OSC"
+
+set scriptpath=%~dp0
+git config --global --add safe.directory %scriptpath%
